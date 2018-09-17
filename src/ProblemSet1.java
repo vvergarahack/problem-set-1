2f6eb6692fdeaed4e6be211ed250670ceeff19a6
Veronica Vergara <veronica.vergara@njwg.cap.gov>
2018-09-17 11:16:53 -0400

    Ex1-5

--- src/ProblemSet1.java
+++ src/ProblemSet1.java
@@ -14,14 +14,44 @@ public class ProblemSet1 {
 	 */
 	
 	public static void main(String[] args) {
-		System.out.println("/// EXERCISE 1 /////////////////////////////////////////////////\n");
+System.out.println("/// EXERCISE 1 /////////////////////////////////////////////////\n");
+		
+		System.out.println("   ##    ######  ######## ########");
+		System.out.println("  #  #   #     # #        #");
+		System.out.println(" #    #  #     # #        #");
+		System.out.println("######## ######  #        ########");
+		System.out.println("#      # #       #               #");
+		System.out.println("#      # #       #               #");
+		System.out.println("#      # #       ######## ########\n");
 		
 		System.out.println("/// EXERCISE 2 /////////////////////////////////////////////////\n");
 		
+		System.out.println("  /\\     /\\     /\\");
+		System.out.println(" /  \\   /  \\   /  \\");
+		System.out.println("/    \\ /    \\ /    \\");
+		System.out.println("\\    / \\    / \\    /");
+		System.out.println(" \\  /   \\  /   \\  /");
+		System.out.println("  \\/     \\/     \\/\n");
+		
 		System.out.println("/// EXERCISE 3 /////////////////////////////////////////////////\n");
+		
+		System.out.println("   \\\\\\\\\\");
+		System.out.println("  +\"\"\"\"\"+");
+		System.out.println("(|  0 0  |)");
+		System.out.println(" |   ^   |");
+		System.out.println(" |  '-'  |");
+		System.out.println("  +-----+\n");
 		
 		System.out.println("/// EXERCISE 4 /////////////////////////////////////////////////\n");
 		
+		System.out.println("H E L L O");
+		System.out.println("E       A");
+		System.out.println("L       P");
+		System.out.println("L       C");
+		System.out.println("O A P C S\n");
+		
 		System.out.println("/// EXERCISE 5 /////////////////////////////////////////////////\n");
+		
+		System.out.println("\"The answer's quite simple,\" the professor said. \"Just think outside the box!\"");
 	}
 }
